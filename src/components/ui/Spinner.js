ae51473abe8ef5611f4ec13b867d893b0847429e
Mahin Sagotra <mahinsagotra18@gmail.com>
2020-11-29 22:35:54 +0530

    Deployed

--- src/components/ui/Spinner.js
+++ src/components/ui/Spinner.js
@@ -3,7 +3,7 @@ import spinner from '../../img/spinner.gif'
 
 const Spinner = () => {
     return (
-        <img src={spinner} style={{ width: '200px', margin: 'auto', display: 'block', alignItems: 'Loading' }} />
+        <img src={spinner} style={{ width: '200px', margin: 'auto', display: 'block', alignItems: 'Loading' }} alt='Spinner' />
     )
 }
 
