bcd29785853b6409ce1621c4bec05ab0639f4020
Mahin Sagotra <mahinsagotra18@outlook.com>
2021-05-20 12:22:43 +0530

    Analytics Added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const injectGA = () => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'GA_TRACKING_ID');
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <script
+      async
+      src='https://www.googletagmanager.com/gtag/js?id=G-C51N10NTE4'
+    ></script>
+    <script>{injectGA()}</script>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
